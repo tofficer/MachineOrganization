7757e4fee0a0f4a1ebac0b42d5928fe5b0f5e146
Tyler Officer <officer@wisc.edu>
2016-02-02 14:54:50 -0600

    Create game2.c

---
+++ game2.c
@@ -0,0 +1,120 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <assert.h>
+#include <unistd.h>
+
+typedef struct node{
+  int val;
+  struct node *next;
+} Node;
+
+Node * createlist(FILE *fp);
+Node * listadd(Node *head, int intforlist);
+void printlist(Node *head);
+void playgame(Node * listhead);
+int inlist(Node *head, int intvalue);
+
+int main(int argc, char* argv[]){
+  char *inputFile;
+  FILE *fp;
+  Node *head;
+
+  if (argc != 2){
+    fprintf(stderr, "usage: generate <file>\n");
+    exit(1);
+  }
+  
+  inputFile = argv[1];
+  fp = fopen(inputFile, "r");
+  assert(fp != NULL);
+
+  head = createlist(fp);
+
+  if (fclose(fp) == EOF){
+    fprintf(stderr, "closing file failed\n");
+    exit(1);
+  }
+
+  playgame(head);
+
+  return 0;
+}
+
+Node * createlist(FILE *fp){
+  Node * head = NULL;
+  char line[256];
+  int buffer;
+
+  while(fgets(line, sizeof(line), fp)){
+      buffer = atoi(line);
+      head = listadd(head, buffer);
+  }
+if (feof(fp) == 0){
+    fprintf(stderr, "fgets failed\n");
+    exit(1);
+  }
+
+  return head;
+}
+
+Node * listadd(Node *head, int intforlist){
+  Node *temp;
+  temp  = (Node*) malloc(sizeof(Node));
+  if (NULL == temp){
+    fprintf(stderr, "malloc failed\n");
+    exit(1);
+  }
+temp->val = intforlist;
+  temp->next = head;
+  head = temp;
+
+  return head;
+}
+
+void printlist(Node *head){
+  Node * current = head;
+
+  while (current != NULL){
+    printf("%d\n", current -> val);
+    current = current->next;
+  }
+}
+
+void playgame(Node * listhead){
+  int str_to_int;
+  char str[100];
+  int n;
+
+  printf("This game has you guess which integers are in the list.\n");
+  printf("Enter an integer or q to quit.\n");
+  while((n = strncmp(fgets(str, 100, stdin), "q", 1)) != 0){                                                   \
+
+    str_to_int = atoi(str);
+    if (inlist(listhead, str_to_int) == 1){
+      printf("%d is in the list\n", str_to_int);
+    }
+    else {
+      printf("%d is not in the list\n", str_to_int);
+    }
+  }
+
+  printf("Quitting.\n");
+}
+int inlist(Node *head, int intvalue){
+  Node * current = head;
+
+  while (current != NULL){
+    if (current->val == intvalue){
+      return 1;
+      break;
+    }
+    current = current->next;
+  }
+  return 0;
+}
+
+  
